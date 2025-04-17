9f3fd5a3dbb566eba83dd6ef1d640270e70c343f
kaushal kumar <kaushal.k.0014@gmail.com>
2025-04-17 22:31:52 +0530

    initial commit'

---
+++ Payment-Service-API/src/main/java/com/learning/payment/config/PaymentServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.learning.payment.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentServiceApiApplication.class, args);
+	}
+
+}
